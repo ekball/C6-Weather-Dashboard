e9ac3ed0847ab3c6d2fa109198a36d0981c7fff7
ekball <90882418+ekball@users.noreply.github.com>
2021-11-16 12:02:56 -0500

    added the relevant info to the current forecast section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-// search button handler must be written in order to get input before rest of code runs
+// create a variable for the current date and 
+var currentDate = new Date();
+var currentDatePlus1 = new Date(currentDate);
+currentDatePlus1 = currentDatePlus1.setDate(currentDatePlus1.getDate() + 1)
+currentDatePlus1 = currentDatePlus1.toDateString();
+console.log(currentDatePlus1);
 
 function getApi(city) {
 
@@ -13,6 +18,12 @@ function getApi(city) {
         // Use the console to examine the response
         console.log(data);
 
+         // change the date to a more readable string
+         currentDate = currentDate.toDateString();
+
+        // change the name of the city (and add the current date to it) in the top right container that holds current weather conditions for that city
+        var cityName = $('#current-city').text(data.name + " (" + currentDate + ")");
+
         // find the coordinates for longitude and latitude
         var longitude = data.coord.lon;
         console.log(longitude);
@@ -20,40 +31,24 @@ function getApi(city) {
         var latitude = data.coord.lat;
         console.log(latitude);
 
-        var longLatUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,daily&appid=8dc431793fa117a46c92a939890cc1fc"
+        // create new api url that pulls more detailed info (such as UV Index)
+        var longLatUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,minutely&units=imperial&appid=8dc431793fa117a46c92a939890cc1fc"
+
+        // fetch new data from newly generated api url
+        fetch(longLatUrl)
+            .then(function (response) {
+            return response.json();
+            })
+
+            .then(function (data) {
+            // Use the console to examine the response
+            console.log(data);
+
+            currentForecast(data)
+
+            })
 
       })
-
-      
-
-
-
-
-
-    //   var getRepoIssues = function (repo) {
-    //     var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc';
-      
-    //     fetch(apiUrl).then(function (response) {
-    //       if (response.ok) {
-    //         response.json().then(function (data) {
-    //           displayIssues(data);
-      
-    //           // What is this checking for? Under what condition will this be `true`?
-    //           // TODO: Write your answer here
-      
-    //           //// this is looking to see if there are more responses than can be viewed
-    //           //// issues that are the most recent will most likely not show due to the ascending sort
-    //           if (response.headers.get('Link')) {
-    //             displayWarning(repo);
-    //           }
-    //         });
-    //       } else {
-    //         document.location.replace('./index.html');
-    //       }
-    //     });
-      
-    
-
 
 };
 
@@ -72,49 +67,45 @@ $("#search-button").click(function () {
 
 });
 
-
-
-
-
-function futureForecast () {
-
-    getApi();
+function futureForecast (data) {
 
     // find the forecast card elements
         // card for 1 day out
-        var dayOne = $('#day1').append("<li></li>")
+        var dayOne = $('#day-1-date').text(currentDate.getDate() + 1);
 
         // card for 2 days out
-        var dayTwo = $('#day2')
+        var dayTwo = $('#day2').append("<li></li>");
 
         // card for 3 days out
-        var dayThree = $('#day3')
+        var dayThree = $('#day3').append("<li></li>");
 
         // card for 4 days out
-        var dayFour = $('#day4')
+        var dayFour = $('#day4').append("<li></li>");
 
         // card for 5 days out
-        var dayFive = $('#day5')
+        var dayFive = $('#day5').append("<li></li>");
 
-
-
-
-    // Create a list element
-    var listItem = document.createElement('li');
-    var list = document.createElement('ul');
-
-    // Set the text of the list element to the JSON response's .html_url property
-    listItem.textContent = "User: " + data[i].login + " Url: " + data[i].html_url;
-
-    // Append the li element to the id associated with the ul element.
-    list.appendChild(listItem);
-    userContainer.appendChild(list);
 
 };
 
-function currentForecast () {
+function currentForecast (data) {
+
+    // make a new element to attach to header h2 and set the current weather icon to it
+    // $('#current-city').append("<p>" + data.current.weather.icon + "</p>");
+
+    // make a new list item to attach to unordered list and set the current Temp in Farenheit to it
+    $('#current-conditions').append("<li>Temperature: " + data.current.temp + " °F</li>");
+
+    // make a new list item to attach to unordered list and set the current Wind Speed to it
+    $('#current-conditions').append("<li>Temperature: " + data.current.wind_speed + " MPH</li>");
 
 
+    // make a new list item to attach to unordered list and set the current Humidity percentage to it
+    $('#current-conditions').append("<li>Temperature: " + data.current.humidity + " %</li>");
+
+
+    // make a new list item to attach to unordered list and set the current UV Index to it
+    $('#current-conditions').append("<li>Temperature: " + data.current.uv + " °F</li>");
 
 };
 
@@ -122,7 +113,7 @@ function main() {
 
 searchBar();
 
-// getApi(city);
+futureForecast();
 
 };
 
